b577c61a4db8f580be30037a3864cf6d0d9b9663
Ashley Edwards <ashley.elize@gmail.com>
2013-12-17 20:45:04 -0500

    update with new error

--- config/initializers/twitter_credentials.rb
+++ config/initializers/twitter_credentials.rb
@@ -1,10 +1,10 @@
 require 'twitter'
 
 Client = Twitter::REST::Client.new do |config|
-  config.consumer_key = "FPub9X051YFtbYr72URsXA"
-  config.consumer_secret = "rJUvRITZ80gVFZ4ifmS5soNh7Tgvpj7FkeKmUMewlAs"
-  config.access_token = "41286815-IpjQ037XsUizupPn82Pr0FjfZ596Yp8SVR2BgP7ol"
-  config.access_token_secret = "agHvrLm61oKHadnNGbh763v9BKo8YvYK2GH4gDqCiGjNU"
+  config.consumer_key = ENV["consumer_key"]
+  config.consumer_secret = ENV["consumer_secret"]
+  config.access_token = ENV["access_token"]
+  config.access_token_secret = ENV["access_token_secret"]
 end
 
 
