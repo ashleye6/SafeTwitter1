c26510c844bdad95c1dc8aa5dda71de94933decf
Ashley Edwards <ashley.elize@gmail.com>
2013-12-18 21:39:31 -0500

    Fix TVShows table

--- db/migrate/20131215162557_create_phrases.rb
+++ db/migrate/20131215162557_create_phrases.rb
@@ -2,7 +2,7 @@ class CreatePhrases < ActiveRecord::Migration
   def change
     create_table :phrases do |t|
       t.string :text
-      t.belongs_to :tv_show, index: true
+      t.belongs_to :tvshow, index: true
 
       t.timestamps
     end
--- db/migrate/20131215173917_remove_tvshow_from_phrases.rb
+++
@@ -1,9 +0,0 @@
-class RemoveTvshowFromPhrases < ActiveRecord::Migration
-  def up
-    remove_column :phrases, :tvshow, :reference
-  end
-
-  def down
-    add_column :phrases, :tvshow, :reference
-  end
-end
