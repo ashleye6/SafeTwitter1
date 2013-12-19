0761179b704f4f6ed61b811ce1a91fffe62e0a59
Ashley Edwards <ashley.elize@gmail.com>
2013-12-18 20:58:45 -0500

    fixed omniauth bad authentication error

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -4,6 +4,14 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   # provider :developer unless Rails.env.production?
   # provider :twitter, ENV['TWITTER_KEY'], ENV['TWITTER_SECRET']
     provider :twitter, ENV['TWITTER_KEY'], ENV['TWITTER_SECRET']
+    {
+      :secure_image_url => 'true',
+      :image_size => 'original',
+      :authorize_params => {
+        :force_login => 'true',
+        :lang => 'pt'
+      }
+    }
 
 
 end
