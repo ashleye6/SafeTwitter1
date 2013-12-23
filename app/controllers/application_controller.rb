4cf4931c93f040a49014e31e3dfe2ba0f834324f
Ashley Edwards <ashley.elize@gmail.com>
2013-12-23 16:30:40 -0500

    fixed rate limit problem

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_filter :load_tweets
+  #before_action :require_login
 
   def load_tweets
     @tweets = Client.home_timeline 
@@ -13,6 +14,17 @@ class ApplicationController < ActionController::Base
       @current_user ||= User.find(session[:user_id]) if session[:user_id]
     end
     helper_method :current_user
+
+    #def require_login
+    #  unless logged_in?
+    #    flash[:error] = "You must be logged in to access this application"
+    #    redirect_to root_url
+    #  end
+    #end
+    #
+    #def logged_in?
+    #  !!current_user
+    #end
 end
 
 #create another model with blocked shows
