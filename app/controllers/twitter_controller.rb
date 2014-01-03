9fecd79a2bb836375ea0706ca1e4ff48f88db2df
Ashley Edwards <ashley.elize@gmail.com>
2014-01-03 09:48:34 -0500

    made code more readable

--- app/controllers/twitter_controller.rb
+++ app/controllers/twitter_controller.rb
@@ -16,8 +16,8 @@ end
   def is_redacted? tweet
     @phrases ||= current_user.blockedshows.map(&:phrases).flatten.map(&:text)
     @phrases.any? { |phrase| tweet.text.include? phrase }
-
   end
+
   helper_method :is_redacted?
   
 end
