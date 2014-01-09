e14dfc2c49d6d925a3a07a31a2c7fd5da7a45fb2
Ashley Edwards <ashley.elize@gmail.com>
2014-01-08 20:40:57 -0500

    changed scandal broken link

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,7 +8,7 @@ tvshow1 = Tvshow.create title: "Breaking Bad",
              image: 'http://www.thecampuscompanion.com/party-lab/files/2012/09/breaking-bad-drinking-game-1024x640.png'
 
 tvshow2 = Tvshow.create title: "Scandal",
-             image: 'http://ia.media-imdb.com/images/M/MV5BMTUwMTQyMTIwMV5BMl5BanBnXkFtZTcwNjI1MzU2Nw@@._V1_.jpg'
+             image: 'http://socialmediatoday.com/sites/socialmediatoday.com/files/imagepicker/360431/scandal_2012_624x351.jpg'
 
 tvshow3 = Tvshow.create title: "Game of Thrones",
              image: 'http://thedrawshop.com/wp-content/uploads/2013/08/game-of-thrones-season-4.jpg'
